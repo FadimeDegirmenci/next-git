cca4b6ba32b62a6f99da81ba4f6cd6f76e8d5f5d
FadimeDegirmenci <fadimedegirmenci@hotmail.com>
2024-09-12 02:51:21 +0300

    navbar

---
+++ firstnextjs/app/components/navbar/CardCound.tsx
@@ -0,0 +1,7 @@
+const CardCound = () => {
+    return(
+     <div>CardCount</div>
+    )
+ }
+ 
+ export default CardCound
--- firstnextjs/app/components/navbar/Navbar.tsx
+++ firstnextjs/app/components/navbar/Navbar.tsx
@@ -1,8 +1,21 @@
 import exp from "constants"
+import Logo from "./logo"
+import Search from "./search"
+import CardCound from "./CardCound"
+import User from "./User"
+import HamburgerMenu from "./HamburgerMenu"
+
 
 const Navbar = () => {
    return(
-    <div>Navbar</div>
+    <div className="flex items-center justify-between gap-3 md:gap-10 px-3 md:px:10 h-16 bg-orange-600">
+    <Logo/>
+    <Search/>
+    <CardCound/>
+    <User/>
+    <HamburgerMenu/>
+
+    </div>
    )
 }
 
