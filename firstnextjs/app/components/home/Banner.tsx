1bbbb07f21092fdf893e479dc8f8eb4ba99f96e9
FadimeDegirmenci <fadimedegirmenci@hotmail.com>
2024-09-17 04:42:38 +0300

    uc

---
+++ firstnextjs/app/components/home/Banner.tsx
@@ -0,0 +1,11 @@
+const Banner = () => {
+    return(
+     <div className="h[237px]">
+        <div className="h[137px]">
+            <Image src="" alt="" />
+        </div>
+     </div>
+    )
+ }
+ 
+ export default Banner
--- firstnextjs/app/components/home/Category.tsx
+++ firstnextjs/app/components/home/Category.tsx
@@ -18,10 +18,10 @@ const Category = () => {
         } 
     ]
    return(
-     <div>
+     <div className="flex items-center justify-center px:3 md:px-10 gap-3 mid:gap-8 overflow-x-auto">
         {
         categoryList.map((category, index) =>(
-            <div key={index}>{category.name}</div>
+            <div className="border text-slate-500 rounded-full min-w-[120px] flex items-center justify-center curser-pointer flex-1 px-4 py-2 text-center" key={index}>{category.name}</div>
         ))
         
         
